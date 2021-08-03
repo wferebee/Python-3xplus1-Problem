3c03b07dfee9041f832f7f3ce2ad373bd343b5c6
William Ferebee <williampferebee@gmail.com>
2021-08-03 11:08:10 -0400

    Initial set up of function and iterator function, now to add charts

---
+++ 3XPlus1Problem.py
@@ -0,0 +1,35 @@
+
+# Running through the first 100 numbers of the 3x + 1 Algebra
+# - If the number is odd, then multiply by (3x+1)
+# - If the number is even, the divide by 2
+# - Stop when the number reaches 1
+# when a number reaches 1, it enters an infinite loop of "1 -> 4 -> 2 -> 1 -> 4..."
+
+# Scientist have worked out that atleast the very first 2^68 digits all reolve to the number loop of "-> 4 -> 2 -> 1 -> 4...",
+# but it has not been proven yet if this exists for all numbers
+
+# Here, I am just running through the first 100 digits as an exercise while learning Python
+
+def ExecuteAlgebra(num):
+    starting_num = num
+    print(f"Starting Num: {starting_num}")
+    steps = 0
+    while num != 1:
+        if(num % 2 == 0):
+            num /= 2
+            print(num)
+            steps += 1
+        else:
+            num = ((num * 3) + 1)
+            print(num)
+            steps += 1
+    print(f"Starting Number: {starting_num}, No. Steps: {steps}")
+
+def ExecuteProblem():
+    for number in range(1, 101):
+        ExecuteAlgebra(number)
+
+
+ExecuteProblem()
+
+#ExecuteAlgebra(17)
